a56165e44ee54590af2256cd780883c8a1dc0ba4
Ben Devos <bendevos07@gmail.com>
2025-11-17 01:33:07 +0100

    feat: bird animation

--- src/main.js
+++ src/main.js
@@ -15,10 +15,18 @@ renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
 
 const treeHouse = new THREE.Group();
+const tree = new THREE.Group();
+
+
+//Pivot for Bird Animation
+const pivot = new THREE.Object3D();
+pivot.position.set(0, 0, 0);
+scene.add(pivot);
 
 //Models
 const modelLoader = new GLTFLoader();
 
+//Torterra (Tree Turtle Pokemon)
 let torterraModel;
 modelLoader.load(
   '/resources/models/torterra/Torterra.gltf',
@@ -35,6 +43,7 @@ modelLoader.load(
   }
 );
 
+//Ladder Model
 let ladderModel;
 modelLoader.load(
   '/resources/models/ladder/scene.gltf',
@@ -43,7 +52,26 @@ modelLoader.load(
     ladderModel.scale.set(0.02, 0.02, 0.02);
     ladderModel.position.set(2, 5, 4);
     scene.add(ladderModel);
-    console.log("ladder model loaded");
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
+
+
+//Talonflame (Bird Pokemon)
+let talonflameModel;
+modelLoader.load(
+  '/resources/models/talonflame/scene.gltf',
+  function (gltf) {
+    talonflameModel = gltf.scene;
+    talonflameModel.position.set(-5, 13, 0);
+    talonflameModel = gltf.scene;
+    talonflameModel.scale.set(0.6, 0.6, 0.6);
+    talonflameModel.rotation.x = degToRad(35);
+
+    pivot.add(talonflameModel);
   },
   undefined,
   function (error) {
@@ -87,6 +115,12 @@ const rooftopMaterial = new THREE.MeshPhongMaterial({
   side: THREE.DoubleSide
 });
 
+const treeFolliageMaterial = new THREE.MeshPhongMaterial({
+  color: 0x144521,
+  shininess: 20,
+  side: THREE.DoubleSide
+});
+
 const portraitMaterials  = [
   new THREE.MeshPhongMaterial({
     color: 0xffffff,
@@ -122,13 +156,7 @@ forestFloor.position.y = -1;
 scene.add( forestFloor );
 
 //Tree house Trunk
-const treeTrunkRadiusTop =  0.7;  
-const treeTrunkRadiusBottom =  0.9;  
-const treeTunkHeight = 15;  
-const treeTrunkRadialSegments = 24;  
-const treeTrunkGeometry = new THREE.CylinderGeometry(
-	treeTrunkRadiusTop, treeTrunkRadiusBottom, treeTunkHeight, treeTrunkRadialSegments
-);
+const treeTrunkGeometry = new THREE.CylinderGeometry(0.7, 0.9, 15, 24);
 const treeTrunk = new THREE.Mesh( treeTrunkGeometry, treeTrunkMaterial );
 
 treeTrunk.position.set(0,7,0);
@@ -195,8 +223,31 @@ const portrait = new THREE.Mesh( portraitGeometry, portraitMaterials);
 portrait.position.set(1,7.5,-2.8);
 treeHouse.add(portrait);
 
-
 scene.add(treeHouse);
+
+//TREES
+
+//conifer trunk
+const coniferTrunkGeometry = new THREE.CylinderGeometry(0.7, 0.9, 5, 24);
+const coniferTrunk = new THREE.Mesh( coniferTrunkGeometry, treeTrunkMaterial);
+coniferTrunk.position.y = 1.5;
+tree.add(coniferTrunk);
+
+//tree folliage
+const treeFolliageGeometry = new THREE.ConeGeometry( 3.3, 5, 16 );
+const treeFolliage1 = new THREE.Mesh(treeFolliageGeometry, treeFolliageMaterial);
+treeFolliage1.position.y = 6;
+const treeFolliage2 = new THREE.Mesh(treeFolliageGeometry, treeFolliageMaterial);
+treeFolliage2.position.y = 7.5;
+treeFolliage2.scale.set(0.8,0.8,0.8);
+const treeFolliage3 = new THREE.Mesh(treeFolliageGeometry, treeFolliageMaterial);
+treeFolliage3.position.y = 9;
+treeFolliage3.scale.set(0.6,0.6,0.6);
+tree.add(treeFolliage1);
+tree.add(treeFolliage2);
+tree.add(treeFolliage3);
+
+
 
 //Scene Lighting
 const ambient = new THREE.AmbientLight(0xffffff, 1);
@@ -213,12 +264,19 @@ scene.add(sun);
 const controls = new OrbitControls( camera, renderer.domElement );
 
 function animate() {
+  requestAnimationFrame( animate );
+
+  //Bird Animation
+  if (talonflameModel) {
+    pivot.rotation.y += 0.0005;
+    talonflameModel.rotation.x = degToRad(35) + Math.sin(Date.now() * 0.001) * degToRad(10);
+  }
 
   renderer.render( scene, camera );
 
 }
 
-function createTriangleGeometry(baseWidth, depth, height, material) {
+const createTriangleGeometry(baseWidth, depth, height, material) => {
   const shape = new THREE.Shape();
   shape.moveTo(-baseWidth / 2, 0);
   shape.lineTo(baseWidth / 2, 0);
@@ -227,4 +285,8 @@ function createTriangleGeometry(baseWidth, depth, height, material) {
   const geom = new THREE.ExtrudeGeometry(shape, { depth: depth, bevelEnabled: false });
   geom.center(); // center geometry so positioning is easier
   return new THREE.Mesh(geom, material);
+}
+
+const randomTreePosition = () => {
+  
 }
